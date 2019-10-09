376159f40adc75eca06d19f5f59bf0c0eaf05899
binaryAccess <binaryAccess@users.noreply.github.com>
2019-10-09 08:52:36 +0200

    Fixed scripts

---
+++ SearchGroup.ps1
@@ -0,0 +1,31 @@
+﻿Function SearchGroupClearTextInformation
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [Array] $Terms,
+
+        [Parameter(Mandatory=$false)]
+        [String] $Domain
+    )
+
+    if ([string]::IsNullOrEmpty($Domain)) {
+        $dc = (Get-ADDomain).RIDMaster
+    } else {
+        $dc = (Get-ADDomain $Domain).RIDMaster
+    }
+
+    $list = @()
+
+    foreach ($t in $Terms)
+    {
+        $list += "(`$_.Description -like `"*$t*`")"
+        $list += "(`$_.Info -like `"*$t*`")"
+    }
+
+    Get-ADGroup -Filter * -Server $dc -Properties Description,Info |
+        Where { Invoke-Expression ($list -join ' -OR ') } | 
+        Select SamAccountName,Description,Info | 
+        fl
+}
+
+SearchGroupClearTextInformation -Terms @("pwd", "pass", "pw", "kodeord")
---
+++ SearchUser.ps1
@@ -0,0 +1,31 @@
+﻿Function SearchUserClearTextInformation
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [Array] $Terms,
+
+        [Parameter(Mandatory=$false)]
+        [String] $Domain
+    )
+
+    if ([string]::IsNullOrEmpty($Domain)) {
+        $dc = (Get-ADDomain).RIDMaster
+    } else {
+        $dc = (Get-ADDomain $Domain).RIDMaster
+    }
+
+    $list = @()
+
+    foreach ($t in $Terms)
+    {
+        $list += "(`$_.Description -like `"*$t*`")"
+        $list += "(`$_.Info -like `"*$t*`")"
+    }
+
+    Get-ADUser -Filter * -Server $dc -Properties Enabled,Description,Info,PasswordNeverExpires,PasswordLastSet |
+        Where { Invoke-Expression ($list -join ' -OR ') } | 
+        Select SamAccountName,Enabled,Description,Info,PasswordNeverExpires,PasswordLastSet | 
+        fl
+}
+
+SearchUserClearTextInformation -Terms @("pwd", "pass", "pw", "kodeord")
